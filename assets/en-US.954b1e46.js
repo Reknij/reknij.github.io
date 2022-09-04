9fa5819a4644754fc12d2ee204df9208699ade79
Reknij <jinker2003@gmail.com>
2022-09-04 20:56:29 +0800

    2022-09-04 20:56:28 Update

---
+++ assets/en-US.954b1e46.js
@@ -0,0 +1,1 @@
+const e={welcome:"Welcome to my Blog!",home:"Home",articles:"Articles",archives:"Archives",categories:"Categories",tags:"Tags",about:"About",back:"Back",postedOn:"Posted on",author:"Author",description:"Description",aboutBlog:"About Blog",lastBuild:"Blog Last Modified Time",nextArticle:"Next",previousArticle:"Previous"};export{e as default};
